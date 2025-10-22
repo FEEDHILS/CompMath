d49bfc15bd434d026f1e01bf21a3ffdaac339000
des0ager <dmitriy1350077@gmail.com>
2025-10-23 00:46:49 +1000

    законченная 2 лаба

--- Newton.py
+++ Newton.py
@@ -1,14 +1,17 @@
 from utils import *
 from math import factorial as fact
 
+
+m = interRes
+
 def NewtonForward(x): # If x is between x_0 and x_1
     result = pointsY[0]
     before = 1
-    
+
     t = (x - pointsX[0])/stepIter # if x = x_0 + th
-    for i in range(1, steps):
-        before *= (t - i + 1)
-        result += FiniteDiff(i) * before / fact(i)
+    for i in range(1, m):
+        before *= (t - (i-1))
+        result += FDTable[(i,0)] * before / fact(i)
 
     return result
 
@@ -17,8 +20,8 @@ def NewtonBackward(x):  # If x is between x_n-1 and x_n
     before = 1
     
     t = (x - pointsX[-1])/stepIter
-    for i in range(1, steps):
-        before *= (t + i - 1)
-        result += FiniteDiff(i, steps - i - 1) * before / fact(i)
+    for i in range(1, m):
+        before *= (t + (i-1))
+        result += FDTable[(i,points-1-i)] * before / fact(i)
 
     return result
